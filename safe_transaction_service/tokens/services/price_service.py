5f80d043eb2ecebc25e18aefbea193baaa46af25
Slava Startsev <slava.startsev@tuta.io>
2022-11-15 20:17:27 +0300

    chore: request gton price from coingecko

--- safe_transaction_service/tokens/services/price_service.py
+++ safe_transaction_service/tokens/services/price_service.py
@@ -183,7 +183,7 @@ class PriceService:
         return self.kucoin_client.get_cro_usd_price()
 
     def get_gton_usd_price(self) -> float:
-        return self.kucoin_client.get_gton_usd_price()
+        return self.coingecko_client.get_gton_usd_price()
 
     @cachedmethod(cache=operator.attrgetter("cache_eth_price"))
     @cache_memoize(60 * 30, prefix="balances-get_eth_usd_price")  # 30 minutes
